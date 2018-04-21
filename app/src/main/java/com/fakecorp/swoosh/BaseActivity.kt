00a50ca2833996098ca4db7e78c919878f0446a7
Disane <tibielias@gmail.com>
2018-04-21 14:29:29 +0200

    lesson 6

---
+++ app/src/main/java/com/fakecorp/swoosh/BaseActivity.kt
@@ -0,0 +1,51 @@
+package com.fakecorp.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity()
+{
+    companion object
+    {
+        // val TAG : String? = BaseActivity::class.java.name
+        const val TAG : String = "LifeCycle"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        Log.d(TAG, "${javaClass.simpleName} onCreate")
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_base)
+    }
+
+    override fun onStart(){
+        Log.d(TAG, "${javaClass.simpleName} onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} onResume")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        super.onDestroy()
+    }
+}
